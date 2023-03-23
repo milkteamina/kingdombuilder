2ce27dc386babd5b1e71b640ee7ad6c5e5ece8af
mina <k1206791@students.katyisd.org>
2023-03-23 13:05:14 -0500

    #mina

--- Frame.java
+++ Frame.java
@@ -1,13 +1,16 @@
 import javax.swing.JFrame;
 
 public class Frame extends JFrame {
-    private static final int width = 1000;
-    private static final int height = 400;
+    
+    private static final int width = 1200;
+    private static final int height = 650;
     public Frame(String framename)
 {
     super(framename);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setSize(width, height);
+    setVisible(true);
+    add(new Panel());
     setVisible(true);
 }
 

db4ca3b06f5d79f44739dff7711959fd96f3c8e6
finlandiaben <88502041+finlandiaben@users.noreply.github.com>
2023-04-04 13:26:04 -0500

    stuff

--- src/BasicHex.java
+++ src/BasicHex.java
@@ -1,13 +1,27 @@
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
 public class BasicHex extends Hex{
     private String terrainType;
     Settlement settlement;
 
-    public BasicHex(String terrainType){
-        super();
+    public BasicHex(int x, int y, ArrayList<Hex> neighbors, String terrainType){
+        super(x, y, neighbors);
         this.terrainType = terrainType;
     }
 
     public void placeSettlement(Settlement settlement){
         this.settlement = settlement;
+    }
+
+    public boolean isPlacable(){
+        //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    }
+
+    public void draw(Graphics g){
+        if(settlement != null){
+            settlement.draw(g, getX() + getSettlementSpacingX(), getY() + getSettlementSpacingY());
+        }
     }
 }

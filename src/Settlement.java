b6301f0bb174790e3c697f8825072039b16e5fde
finlandiaben <88502041+finlandiaben@users.noreply.github.com>
2023-04-04 13:27:41 -0500

    Stuff

--- src/Settlement.java
+++ src/Settlement.java
@@ -6,14 +6,28 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class Settlement {
+
     private Player owner;
     private BufferedImage image;
+    private static final int imageSizeX = 5;
+    private static final int imageSizeY = 5;
 
     public Settlement(Player player){
         try{
             owner = player;
 
+            if(player.getId() == 0){
+                image = ImageIO.read(Settlement.class.getResource("/Images/KB-Player1Settlement.png"));
+            } else if(player.getId() == 1){
+                image = ImageIO.read(Settlement.class.getResource("/Images/KB-Player2Settlement.png"));
+            } else if(player.getId() == 2){
+                image = ImageIO.read(Settlement.class.getResource("/Images/KB-Player3Settlement.png"));
+            } else if(player.getId() == 3){
+                image = ImageIO.read(Settlement.class.getResource("/Images/KB-Player4Settlement.png"));
+            }
 
+        }catch(Exception e){
+            System.out.println("Settlement constructor failed due to IO exception");
         }
     }
 
@@ -22,6 +36,6 @@ public class Settlement {
     }
 
     public void draw(Graphics g, int x, int y){
-
+        g.drawImage(image, x, y, imageSizeX, imageSizeY, null);
     }
 }

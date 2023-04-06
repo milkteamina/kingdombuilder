093ac97921b0bd242248162ae2b92a68382a0f76
Andrew <117681305+A4dy@users.noreply.github.com>
2023-04-06 13:11:36 -0500

    Update ObjectiveCard.java

--- src/ObjectiveCard.java
+++ src/ObjectiveCard.java
@@ -1,6 +1,6 @@
 import java.awt.Graphics;
 
 public interface ObjectiveCard {
-    void scorePlayer(Board board, Player player);
+    void scorePlayer(Board board, Player player); //test
     void draw(Graphics g);
 }

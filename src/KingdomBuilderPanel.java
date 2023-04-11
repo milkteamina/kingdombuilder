eb7d2b3ee51902e7581b32c5680f7cb5a5adf5e5
milkteamina <126199186+milkteamina@users.noreply.github.com>
2023-04-11 13:25:44 -0500

    draw objective cards!

--- src/KingdomBuilderPanel.java
+++ src/KingdomBuilderPanel.java
@@ -8,22 +8,37 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class KingdomBuilderPanel extends JPanel implements MouseListener{
-
+   
     private Gamestate g;
     private BufferedImage background;
     private int objectiveCardSpacing;
+    private BufferedImage miners;
+    private BufferedImage knights;
+    private BufferedImage merchants;
+    private BufferedImage cardBack;
 
     public KingdomBuilderPanel() {
-
+         try{
+           background = ImageIO.read(KingdomBuilderPanel.class.getResource("/Image/background.png");
+           miners = ImageIO.read(KingdomBuilderPanel.class.getResource("/Image/KB-miners.png");
+           knights = ImageIO.read(KingdomBuilderPanel.class.getResource("/Image/KB-knights.png");
+           merchants = ImageIO.read(KingdomBuilderPanel.class.getResource("/Image/KB-merchants.png");
+           cardBack = ImageIO.read(KingdomBuilderPanel.class.getResource("/Image/KB-Card-Back.png");
+        } catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public void paint(Graphics g) {
         g.clearRect(0, 0, getWidth(), getHeight()); //so panel doesn't get fucked up
-
+        g.drawImage(background, 0, 0, null);
     }
 
     public void drawObjectiveCards(Graphics g) {
-
+        g.drawImage(miners, 50, 50, 100, 150, null);
+        g.drawImage(knights, 165, 50, 100, 150, null);
+        g.drawImage(merchants, 280, 50, 100, 150, null);
+       
     }
 
     public void drawScoring(Graphics g) {
@@ -31,7 +46,8 @@ public class KingdomBuilderPanel extends JPanel implements MouseListener{
     }
 
     public void drawDeck(Graphics g) {
-
+        g.drawImage(cardBack, 600, 100, 100, 150, null);
+       
     }
 
     @Override

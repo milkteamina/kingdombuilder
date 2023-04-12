c02583cc4265c0800e90ae4f9607d42c43c0b82a
Andrew <117681305+A4dy@users.noreply.github.com>
2023-04-12 13:26:54 -0500

    Add files via upload

---
+++ src/ScoreCards.java
@@ -0,0 +1,63 @@
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+
+public class ScoreCards implements ObjectiveCard{
+    private static final int SIZEX = 200;
+    private static final int SIZEY = 350;
+    private int x;
+    private int y;
+    private BufferedImage minersFace;
+    private BufferedImage merchantsFace;
+    private BufferedImage knightsFace;
+    private int cardSpacingX;
+    
+    public ScoreCards(int x2, int y2)
+    {
+        try {
+            x = x2;
+            y = y2;
+            minersFace = ImageIO.read(Deck.class.getResource("/Images/KB-miners.png"));
+            knightsFace = ImageIO.read(Deck.class.getResource("/Images/KB-knights.png"));
+            merchantsFace = ImageIO.read(Deck.class.getResource("/Images/KB-merchants.png"));
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public int minerScore(Board board, Player player) {
+        int score = 0;
+        ArrayList<Hex> allHexes = board.getAllHexes();
+        for(Hex x : allHexes)
+        {
+            if(x.getSettlement().getOwner().getId() == player.getId())
+            {
+                ArrayList<Hex> neighborHexes = x.getNeighbors();
+                for(Hex i : neighborHexes)
+                    if(i.getTerrainType().equals("mountain"))
+                        score += 1;
+            }
+        }
+        return score;
+    }
+    
+    public int merchantScore(Board board, Player player) {
+        int score = 0;
+        return score;
+    }
+    
+    public int knightScore(Board board, Player player) {
+        int score = 0;
+        return score;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(minersFace, x, y, SIZEX, SIZEY, null);
+        g.drawImage(merchantsFace, x+cardSpacingX, y, SIZEX, SIZEY, null);
+        g.drawImage(knightsFace, x+(2 * cardSpacingX), y, SIZEX, SIZEY, null);
+    }
+}

0207b4421b10f209aae35fe3d88e020d1bb5c29a
milkteamina <126199186+milkteamina@users.noreply.github.com>
2023-04-19 12:58:07 -0500

    added set available moves

--- src/Tavern.java
+++ src/Tavern.java
@@ -14,7 +14,28 @@ public class Tavern implements ExtraAction{
     private BufferedImage image;;
 
     public void setAvailableMoves(Board board, Player player){
-
+                //probably not the best way to do this 
+        for(Hex h: board.getAllHexes()){
+             if(h.getSettlement() == null){//looks at all the free hexes
+              for(Hex i: h.getNeighbors()){
+                if(i.getSettlement() != null){ // starts the 3 settlement chain 
+                  for(Hex j: i.getNeighbors()){
+                    if(j.getSettlement() != null){ // checks for the second settlement
+                        for(Hex k : j.getNeighbors()){
+                         if(k.getSettlement() != null){ //checks for a third connected settlement
+                            if(h.highlight()== false){
+                                h.highlight();
+                            }
+                         }
+                        }
+                    }
+                  }
+                }
+              }
+                 
+             }  
+             
+          }
     }
 
     @Override

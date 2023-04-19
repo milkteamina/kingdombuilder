e6ab524a4634447928c6ae612c281e6197c012b6
milkteamina <126199186+milkteamina@users.noreply.github.com>
2023-04-19 12:56:15 -0500

    fixed something

--- src/Harbor.java
+++ src/Harbor.java
@@ -28,26 +28,7 @@ public class Harbor implements ExtraAction{
 
     }
     public void setAvailableMoves(Board board, Player player, Hex hex){
-        //probably not the best way to do this 
-       /* for(Hex h: board.getAllHexes()){
-             if(h.getSettlement() == null){//looks at all the free hexes
-              for(Hex i: h.getNeighbors()){
-                if(i.getSettlement() != null){ // starts the 3 settlement chain 
-                  for(Hex j: i.getNeighbors()){
-                    if(j.getSettlement() != null){ // checks for the second settlement
-                        for(Hex k : j.getNeighbors()){
-                         if(k.getSettlement() != null){ //checks for a third connected settlement
-                            h.highlight();   
-                         }
-                        }
-                    }
-                  }
-                }
-              }
-                 
-             }  
-             
-          }*/
+
          
     }
 

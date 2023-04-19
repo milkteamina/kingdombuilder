8879e1114c4abbad3859a405a088fd6e462cfbb4
finlandiaben <88502041+finlandiaben@users.noreply.github.com>
2023-04-19 10:10:30 -0500

    Create ScoreCard.java

---
+++ src/ScoreCard.java
@@ -0,0 +1,133 @@
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+
+public class ScoreCards implements ObjectiveCard{
+    private static final int SIZEX = 200;
+    private static final int SIZEY = 350;
+    private int x;
+    private int y;
+    private BufferedImage minersFace;
+    private BufferedImage merchantsFace;
+    private BufferedImage knightsFace;
+    private int cardSpacingX;
+    
+    public ScoreCards(int x2, int y2)
+    {
+        try {
+            x = x2;
+            y = y2;
+            minersFace = ImageIO.read(Deck.class.getResource("/Images/KB-miners.png"));
+            knightsFace = ImageIO.read(Deck.class.getResource("/Images/KB-knights.png"));
+            merchantsFace = ImageIO.read(Deck.class.getResource("/Images/KB-merchants.png"));
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public int minerScore(Board board, Player player) {
+        int score = 0;
+        ArrayList<Hex> allHexes = board.getAllHexes();
+        for(Hex x : allHexes)
+        {
+            if(x.getSettlement().getOwner().getId() == player.getId())
+            {
+                ArrayList<Hex> neighborHexes = x.getNeighbors();
+                for(Hex i : neighborHexes)
+                    if(i.getType().equals("mountain"))
+                        score += 1;
+            }
+        }
+        return score;
+    }
+    
+    public int merchantScore(Board board, Player player) {
+        int score = 0;
+        ArrayList<Hex> allHexes = board.getAllHexes();
+        ArrayList<Hex> playerHexies = new ArrayList<Hex>();
+        ArrayList<Hex> urSoHexy = new ArrayList<Hex>();
+
+        for(Hex x : allHexes) //part 1 - Getting an arraylist of locations that have a neighboring settlement owned by the player (to save on run time).
+        {
+            if(x.getSettlement().getOwner().getId() == player.getId())
+                playerHexies.add(x);
+        }
+        for(Hex h : playerHexies)
+        {
+            ArrayList<Hex> tempNeighbors = h.getNeighbors();
+            for(Hex z : tempNeighbors)
+            {
+                String type = z.getType();
+                if(type.equals("city") || type.equals("oasis") || type.equals("paddock") || type.equals("tavern") || type.equals("harbor"))
+                    urSoHexy.add(z);
+            }
+        }
+
+        if(urSoHexy.size() < 2)
+            return 0;
+        
+        boolean connected = false; //part 2 - Checking if each location is connected to another location, and adding 4 to score if it is.
+
+        for(Hex hix : urSoHexy)
+        {
+            if(connected)
+            {
+                score+=4;
+                connected = false;
+            }
+            for(Hex hox : urSoHexy)
+                if(!connected)
+                    if(!hox.equals(hix))
+                    {
+                        visited.clear();
+                        connected = isConnected(board, player, hix, urSoHexy);
+                    }
+        }
+        return score;
+    }
+    private ArrayList<Hex> visited = new ArrayList<Hex>();
+
+    public boolean isConnected(Board b, Player p, Hex location1, ArrayList<Hex> otherSettlements) //extention to merchantScore to get recursion working
+    {
+        ArrayList<Hex> neighbors = location1.getNeighbors();
+
+        visited.add(location1);
+        for(Hex neighbor : neighbors)
+        {
+            if(!visited.contains(neighbor)) {
+                if(otherSettlements.contains(neighbor))
+                    return true;
+                if(neighbor.getSettlement().getOwner().getId() == p.getId())
+                    if(isConnected(b, p, neighbor, otherSettlements))
+                        return true;
+            }
+        }
+        return false;
+    }
+    
+    public int knightScore(Board board, Player player) {
+        int score = 0; 
+        ArrayList<Hex> allHexes = board.getAllHexes();
+        for(int x = 0; x < 20; x++)
+        {
+            int tempScore = 0;
+            for(int y = 0; x < 20; y++)
+            {
+                if(allHexes.get((x*20)+y).getSettlement().getOwner().getId() == player.getId())
+                    tempScore+=2;
+            }
+            score = Math.max(tempScore, score);
+        }
+        return score;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(minersFace, x, y, SIZEX, SIZEY, null);
+        g.drawImage(merchantsFace, x+cardSpacingX, y, SIZEX, SIZEY, null);
+        g.drawImage(knightsFace, x+(2 * cardSpacingX), y, SIZEX, SIZEY, null);
+    }
+}
